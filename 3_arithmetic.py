68e44221dd1c98d05c197f885164b7fc5fffe0c0
Aniruddha-456 <anirudpande456@gmail.com>
2025-09-28 13:27:53 +0530

    arithmetic operations

---
+++ 3_arithmetic.py
@@ -0,0 +1,34 @@
+import math
+
+a=3.14
+b=-4
+c=5
+
+z = round(a)
+y = abs(b)
+power = pow(4,3)
+maxi = max(10,20,30)
+mini = min(10,20,30)
+
+
+print(z)
+print(y)
+print(power)
+print(maxi)
+print(mini)
+
+
+
+#math functions
+
+x= 9.5
+y=25
+print(math.pi)
+print(math.e)
+result1 = math.sqrt(y)
+result2 = math.ceil(x)
+result3 = math.floor(x)
+
+print(result1)
+print(result2)
+print(result3)
