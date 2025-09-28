fad95c9ac5f6205be64f6fd900b8c3ded3d0d853
Aniruddha-456 <anirudpande456@gmail.com>
2025-09-28 12:59:55 +0530

    learning input methods

---
+++ 2_input.py
@@ -0,0 +1,8 @@
+# input() = A function that prompts user to input some data
+# default return format is string
+
+name = input(" what is your name :  ")
+age = int(input("what is you age : "))
+
+print(f'My name is {name}')
+print(f'my age is {age} now ******damn it')
