32d16928bf951ef60c684d81c3da37e7b3fc4451
Aniruddha-456 <anirudpande456@gmail.com>
2025-09-28 12:30:32 +0530

    Initial programs

---
+++ variable.py
@@ -0,0 +1,26 @@
+# lets use 'f' string
+
+# Srings
+name = "aniruddha"
+food = "Pav bhaji"   #print(food)
+
+print (f'My name is {name}.', f'\nI like to have {food}.')
+
+# Integer 
+age = 25
+year = 2000
+
+print(f'I am {age} years old,', f'as i was born in the year {year}')
+
+# Float
+grade = 9.3
+
+print(f'My grade in Engg was {grade}')
+
+# Boolean
+is_student = True
+
+if is_student :
+    print(f'aniruddha is a student')
+else :
+    print('he is not a student')
