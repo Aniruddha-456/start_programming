ec45b53139a8fbda8766a93cdd423b0c361075e4
Aniruddha-456 <anirudpande456@gmail.com>
2025-09-28 13:12:36 +0530

    added some exercises

---
+++ -ex_2.py
@@ -0,0 +1,9 @@
+# shopping cart program 
+
+food = input("enter the food you want fellow : ")
+price = float(input("what is the price : "))
+number = int(input("how many do you want : "))
+amount = price * number 
+
+
+print(f'if you are looking for {food}', f'\n It costs {price}', f'The total amount is {amount}')
---
+++ _ex_1.py
@@ -0,0 +1,8 @@
+# calculate the area of rectangle ask the inputs to the user and print appropriately
+
+len = float(input(" Enter the length : "))
+wid = float(input("Enter the width : "))
+
+area = len * wid 
+
+print(f"The area of triangle with length = {len}cm and width = {wid}cm is : {area}sq.cm")
