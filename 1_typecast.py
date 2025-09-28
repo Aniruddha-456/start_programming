8b31e4af36ad6eb3a0053292dd0e0985bfa45edd
Aniruddha-456 <anirudpande456@gmail.com>
2025-09-28 12:45:15 +0530

    typecast_init

---
+++ 1_typecast.py
@@ -0,0 +1,12 @@
+# typecasting : the process of converting a variable from one datatype to another
+#    str(), int(), float(), bool()
+
+name = "aniruddha"
+age = 25
+gpa = 9.4
+is_lazy = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+type(type(is_lazy))
